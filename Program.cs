c77b7679967f10b9ca0ae070327fac3f8d56af25
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 22:13:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp_Predict_Output_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+            int x = 10;
+            result = (!(x == 10));
+            Console.WriteLine("Result : " + result); //False
+        }
+    }
+}
